cb89195819b48f73113c441b833cc530d50b4942
camsongiang-admin <camsongiang@gmail.com>
2022-01-04 11:20:48 -0500

    update reference to logo

--- lib/widgets/top_nav.dart
+++ lib/widgets/top_nav.dart
@@ -18,7 +18,7 @@ AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key,
                 Container(
                     padding: const EdgeInsets.only(left: 14),
                     child: Image.asset(
-                      './assets/icons/logo.png',
+                      'assets/assets/icons/logo.png',
                       width: 28,
                     ))
               ],
