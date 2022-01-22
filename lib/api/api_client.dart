53846777dbac958ecfbab7e26f249a3d064fcb7f
camsongiang-admin <camsongiang@gmail.com>
2022-01-22 10:46:37 -0500

    update the configuration for local to point to local api

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -9,50 +9,56 @@ import 'package:web_ui/models/token_info.dart';
 import 'package:web_ui/models/token_model.dart';
 import 'package:web_ui/utils/shared_preferences_manager.dart';
 
-
 class ApiClient {
   static Future<Token> getOauth2Token(grantType, code, redirectUri) async {
-      final queryParameters = <String, dynamic>{
-        r'grant_type': grantType,
-        r'code': code,
-        r'redirect_uri': redirectUri
-      };
-      var _response = await http.post(Uri.https('${dotenv.env['API_NAME']}', 'v1/oauth2/token', queryParameters));
-      print("Response" + _response.toString());
-      var _token = tokenFromJson(_response.body);
-      return _token;
+    final queryParameters = <String, dynamic>{
+      r'grant_type': grantType,
+      r'code': code,
+      r'redirect_uri': redirectUri
+    };
+    var _response = await http.post(getUri('${dotenv.env['API_NAME']}',
+        '${dotenv.env['API_VERSION']}/oauth2/token', queryParameters));
+    print("Response" + _response.toString());
+    var _token = tokenFromJson(_response.body);
+    return _token;
   }
 
   static Future<Pools> getOpenPools() async {
     Map<String, dynamic> qParams = {
-      'status':'OPEN',
+      'status': 'OPEN',
     };
-    var _response = await http.get(Uri.https('${dotenv.env['API_NAME']}', 'v1/pools', qParams));
+    var _response = await http.get(getUri('${dotenv.env['API_NAME']}',
+        '${dotenv.env['API_VERSION']}/pools', qParams));
     var _pools = PoolsFromJson(_response.body);
     return _pools;
   }
 
   static Future<String> getCoinbaseUrl(Map data) async {
     var body = json.encode(data);
-    final SharedPreferencesManager _sharedPreferencesManager = locator<SharedPreferencesManager>();
-    if (_sharedPreferencesManager.isKeyExists(SharedPreferencesManager.keyAccessToken)) {
-      String? token = _sharedPreferencesManager.getString(SharedPreferencesManager.keyAccessToken);
+    final SharedPreferencesManager _sharedPreferencesManager =
+        locator<SharedPreferencesManager>();
+    if (_sharedPreferencesManager
+        .isKeyExists(SharedPreferencesManager.keyAccessToken)) {
+      String? token = _sharedPreferencesManager
+          .getString(SharedPreferencesManager.keyAccessToken);
       print(token);
-      var _qParams = {
-        'Authorization': 'Bearer ' + token!
-      };
+      var _qParams = {'Authorization': 'Bearer ' + token!};
       var _response = await http.post(
-        Uri.https('${dotenv.env['API_NAME']}', 'v1/payments/coinbase'), 
-        body:body,
-        headers: _qParams
-      );
+          getUri('${dotenv.env['API_NAME']}',
+              '${dotenv.env['API_VERSION']}/payments/coinbase'),
+          body: body,
+          headers: _qParams);
       var decodedResp = json.decode(_response.body);
       return decodedResp["data"]["transaction_url"];
     }
     return "";
   }
 
-
-
-  
+  static Uri getUri(String authority, String unencodedPath,
+      [Map<String, dynamic>? queryParameters]) {
+    if (authority.contains("localhost"))
+      return Uri.http(authority, unencodedPath, queryParameters);
+    else
+      return Uri.https(authority, unencodedPath, queryParameters);
+  }
 }
