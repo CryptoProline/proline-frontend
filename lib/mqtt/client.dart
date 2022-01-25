01e1b36ed57cb374b017693d7b6f2030b03226d8
DESKTOP-4QV1HO9\jason <jasongill2000@hotmail.com>
2022-01-25 09:32:09 -0500

    Mqtt test

--- lib/mqtt/client.dart
+++ lib/mqtt/client.dart
@@ -5,10 +5,12 @@
 import 'package:jwt_decode/jwt_decode.dart';
 import 'package:mqtt_client/mqtt_browser_client.dart';
 import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 import 'package:web_ui/injector/injector.dart';
 import 'package:web_ui/mqtt/consts.dart';
 import 'package:web_ui/utils/shared_preferences_manager.dart';
-import 'package:sigv4/sigv4.dart';
+// import 'package:sigv4/sigv4.dart';
+import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
 import 'package:amazon_cognito_identity_dart_2/cognito.dart';
 
 class AWSIotDevice {
@@ -56,15 +58,30 @@ class AWSIotDevice {
     print("secretKey: " + credentials['secretKey']!);
     print("sessionToken: " + credentials['sessionToken']!);
     var url = await getWebSocketUrl();
-    var client = MqttBrowserClient(url, clientId);
+    var client = MqttBrowserClient(url, '');
+    // var client = MqttBrowserClient(url, 'flutter');
     client.logging(on: enableLogging);
+    client.websocketProtocols = ['mqtt', "x-amzn-mqtt-ca", "NA", 'wss'];
     client.port = 443;
+    // client.port = 8883;
+    client.setProtocolV311();
     client.connectionMessage =
         MqttConnectMessage().withClientIdentifier(clientId).keepAliveFor(300);
     client.keepAlivePeriod = 300;
+
+    print("Before get Token param");
+    
     
     var tokenParam = getTokenParam();
-
+    print("after get Token param");
+  // final connMessage = MqttConnectMessage()
+  //     .withClientIdentifier('Mqtt_spl_id')
+  //     .keepAliveFor(60) // Must agree with the keep alive set above or not set
+  //     .withWillTopic('willtopic') // If you set this you must set a will message
+  //     .withWillMessage('My Will message')
+  //     .startClean() // Non persistent session for testing
+  //     .authenticateAs(tokenParam["username"], tokenParam["token"])
+  //     .withWillQos(MqttQos.atLeastOnce);
     // final connMessage = MqttConnectMessage()
     //   .withClientIdentifier('Mqtt_MyClientUniqueId')
     //   .withWillMessage('My Will message')
@@ -77,7 +94,10 @@ class AWSIotDevice {
     try {
       print("ABOUT TO CONNNNECCCTTT");
       await client.connect();
-    } on Exception catch (e) {
+      // await client.connect();
+      print("AFTER CONNNNECCCTTT");
+    } catch (e) {
+      print('EXAMPLE::client exception - $e');
       client.disconnect();
       throw e;
     }
@@ -99,29 +119,30 @@ class AWSIotDevice {
       'X-Amz-Algorithm': aws4HmacSha256,
       'X-Amz-Credential': creds.join('/'),
       'X-Amz-Date': now,
+      'X-Amz-Expires': '86400',
       'X-Amz-SignedHeaders': 'host',
     };
-    var canonicalQueryString = Sigv4.buildCanonicalQueryString(queryParams);
-    var request = Sigv4.buildCanonicalRequest(
+    var canonicalQueryString = SigV4.buildCanonicalQueryString(queryParams);
+    var request = SigV4.buildCanonicalRequest(
       'GET',
       urlPath,
       queryParams,
       {'host': endpoint},
       '',
     );
-    var hashedCanonicalRequest = Sigv4.hashPayload(request);
-    var stringToSign = Sigv4.buildStringToSign(
+    var hashedCanonicalRequest = SigV4.hashCanonicalRequest(request);
+    var stringToSign = SigV4.buildStringToSign(
       now,
-      Sigv4.buildCredentialScope(now, region, serviceName),
+      SigV4.buildCredentialScope(now, region, serviceName),
       hashedCanonicalRequest,
     );
-    var signingKey = Sigv4.calculateSigningKey(
+    var signingKey = SigV4.calculateSigningKey(
       credentials['secretKey']!,
       now,
       region,
       serviceName,
     );
-    var signature = Sigv4.calculateSignature(signingKey, stringToSign);
+    var signature = SigV4.calculateSignature(signingKey, stringToSign);
     var securityTokenParam = Uri.encodeComponent(credentials['sessionToken']!);
     var finalParams = "$canonicalQueryString&X-Amz-Signature=$signature&X-Amz-Security-Token=$securityTokenParam";
 
